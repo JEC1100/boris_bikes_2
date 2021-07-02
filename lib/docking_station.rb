da8950c01b90eb73f667726e8542c96dcb5879d3
jamescondon <jecondon66@icloud.com>
2021-07-02 17:43:32 +0100

    Added full dock guard clause

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,12 @@ require_relative "bike"
 class DockingStation
   attr_reader :bike
     def release_bike
-        bike ? @bike : (fail "No bike")
+        @bike ? @bike : (fail "No bike")
     end
 
     def dock(bike)
-      @bike = bike
+      @bike ? (fail "Dock full"): @bike = bike
     end
 end
+
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,17 +10,17 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to(:bike) }
     it "docks bike" do
-        bike = Bike.new
-        expect(subject.dock(bike)).to eq bike
+        expect(subject.dock(Bike.new)).to eq bike
     end
     it "returns docked bike" do
-        bike = Bike.new
-        subject.dock(bike)
+        subject.dock(Bike.new)
         expect(subject.bike).to eq bike
     end
     it "raises error when no bike" do
     expect {subject.release_bike}.to raise_error "No bike"
     end
-
-
+    it "raises an error when dock occupied" do
+    subject.dock(Bike.new)
+    expect {subject.dock(bike)}.to raise_error "Dock full"
+    end
 end
